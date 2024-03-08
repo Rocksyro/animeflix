b63575b13ccd71c9e66c23ae5ced6bf10c2d47d3
japsolo <jh.docente@gmail.com>
2024-03-08 10:47:02 -0500

    Slider impprovement :D

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Card.css';
+import "./Card.css";
 
-function Card({ rating, title, image}) {
-  return (
-    <>
-      <div className="card">
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-        <p>{rating}</p>
-      </div>
-    </>
-  )
+function Card({ data }) {
+    const { images, title, rating } = data;
+    const image = images.jpg.image_url;
+    return (
+        <>
+            <div className="card">
+                <img src={image} alt={title} />
+                <h3>{title}</h3>
+                <p>{rating ?? "Sin rating"}</p>
+            </div>
+        </>
+    );
 }
 
 export default Card;
