9a67267b4640215965418fb41b9f4bea6f0c42cd
josemiranda989 <josemiranda989@gmail.com>
2024-03-14 12:56:26 -0300

    first test

--- src/components/Slide/Slide.test.jsx
+++ src/components/Slide/Slide.test.jsx
@@ -1,10 +1,13 @@
 import { describe, expect, test } from "vitest";
-
+import { render, screen } from "@testing-library/react";
+import { Slide } from "./Slide";
 // describe sirve para agrupar distintos test
 describe("Test from <Slide />", () => {
   // test son los test unitarios
   test("should add two numbers", () => {
     // expect sería lo que espera este test
-    expect(1 + 1).toBe(2);
+    // expect(1 + 1).toBe(2);
+    render(<Slide />);
+    expect(screen.getByText(/Loading/i)).toBeDefined();
   });
 });
